6144b1f70d5080007fa8f46176835a0297855d16
nafissazid <nafissazid48@gmail.com>
2022-01-22 16:29:48 +0600

    added convert to array method in helper

--- app/helper/common_helper.rb
+++ app/helper/common_helper.rb
@@ -1,7 +1,11 @@
 module CommonHelper
   def convert_to_array_remove_first
-    people_array = given_format.split(/\n/)
+    people_array = convert_to_array
     people_array.drop(1)
+  end
+
+  def convert_to_array
+    given_format.split(/\n/)
   end
 
   def remove_leading_zeroes(date_attribute)
